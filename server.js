64da6b2d2eca235f95d8e347c903191e8450e670
Lembalemba <franklembalemba3@gmail.com>
2024-11-03 13:07:04 +0200

    forgot to change email

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const localDB = "mongodb://127.0.0.1:27017/my-portfolio"
 const liveDB = "mongodb+srv://odtm00:xoxo1212@tayantpay.00zqadh.mongodb.net"
 mongoose.set('strictQuery', true);
 
-mongoose.connect(localDB,{useNewUrlParser: true}).then(() => {
+mongoose.connect(liveDB,{useNewUrlParser: true}).then(() => {
   console.log('database is connected')
 }).catch((err) => console.log('error connecting to database ', err))
   
@@ -38,7 +38,7 @@ app.set('trust proxy', 1);
 
  // Session configuration
 const sessionStore = MongoStore.create({ 
-  mongoUrl: localDB,
+  mongoUrl: liveDB,
   ttl: 14 * 24 * 60 * 60 // 14 days
 });
 
